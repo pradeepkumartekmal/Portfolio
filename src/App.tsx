040f80706b54f09036379cc30572f4a086ff32fc
pradeeptekmal <pradeeptekmal@gmail.com>
2026-02-26 20:51:34 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CustomCursor from "@/components/CustomCursor";
-import Index from "./src/pages/Index";
-import NotFound from "./src/pages/NotFound";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
